68649eaa7a46d77c123ad4d57c2bfc5d41963b9e
dtmacroh <dtmacroh@ucalgary.ca>
2018-05-05 17:49:22 -0600

    comments added

--- cd.py
+++ cd.py
@@ -1,14 +1,11 @@
+# works with Python 2.7
 
 while True:
-    jack, jill = [int(i) for i in input().split()]
+    jack, jill = [int(i) for i in raw_input().split()]
     sell=0
     if jack==0 and jill==0:
         break
-    jackList =frozenset(int(input()) for _ in range(jack))
-    jillList =frozenset(int(input()) for _ in range(jill))
-    com = 0
+    jackList =set(int(raw_input()) for _ in range(jack))
+    jillList =set(int(raw_input()) for _ in range(jill))
     diff = jackList&jillList
-            
-
     print(len(diff))
-
