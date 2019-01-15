7719b2ccf69283b12994ba36b558faceb3b84f96
dtmacroh <dtmacroh@ucalgary.ca>
2019-01-14 21:37:43 -0700

    adding elevator trouble problem

---
+++ elevatortrouble.py
@@ -0,0 +1,31 @@
+# elevatortrouble
+# Author:		Debbie Macrohon
+# Description:	
+
+vars = [int(i) for i in input().split(" ")]
+floors = vars[0]
+start = vars[1]
+goal = vars[2]
+up = vars[3]
+down = vars[4]
+
+pushes = 0
+going = start
+
+#going up while loop
+while (goal>going) and up >0:
+    going= going + up
+    pushes +=1
+    
+#going down while loop
+while (going > goal) and down >0:
+
+    going = going-down
+    pushes +=1
+
+if going==goal:
+    print(pushes)
+else:
+    print("use the stairs")
+
+    
