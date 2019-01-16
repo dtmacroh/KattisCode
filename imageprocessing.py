b4854f505287870366388db71b1a35558d4d421a
dtmacroh <dtmacroh@ucalgary.ca>
2019-01-15 20:52:00 -0700

    correct initialization of 2d matrix

--- imageprocessing.py
+++ imageprocessing.py
@@ -8,11 +8,18 @@ widthImage = vars[1]
 heightKer = vars[2]
 widthKer = vars[3]
 
-imageArray = []*heightImage
-kerArray = []**heightKer
+imageArray = [[0 for x in range(widthImage)] for y in range(heightImage)] 
+kerArray = [[0 for x in range(widthKer)] for y in range(heightKer)] 
 
 for h in (heightImage):
-    imageArray[h] = input()
+    tempImage = [int{i) for i in input().split(" ")]
+    for t in (tempImage):
+        imageArray[h][t] = tempImage[t]
 
 for r in (heightKer):
-    kerArray[r] = input()
+    tempKernel = [int(i) for i in input().split(" ")]
+    for k in (tempKernel):
+        kerArray[r][k] = tempKernel[k]
+
+
+
