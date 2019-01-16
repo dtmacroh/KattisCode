9c8dda056bb4d56366112514025a71e3116221e1
dtmacroh <dtmacroh@ucalgary.ca>
2019-01-15 20:33:21 -0700

    in progress

---
+++ imageprocessing.py
@@ -0,0 +1,18 @@
+# imageprocessing
+# Author:		Debbie Macrohon
+# Description:	
+
+vars = [int(i) for i in input().split(" ")]
+heightImage = vars[0]
+widthImage = vars[1]
+heightKer = vars[2]
+widthKer = vars[3]
+
+imageArray = []*heightImage
+kerArray = []**heightKer
+
+for h in (heightImage):
+    imageArray[h] = input()
+
+for r in (heightKer):
+    kerArray[r] = input()
