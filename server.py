9b70455ff752b67495a98a2f25352a61455532b7
Debbie Macrohon <DMacrohon@bridgewaterbank.ca>
2019-01-21 17:35:32 -0700

    adding server problem

---
+++ server.py
@@ -0,0 +1,16 @@
+# Server
+# Author:		Debbie Macrohon
+# Description:	iteratively deduct times until total time - cumulative(times) become negative
+
+vars = [int(i) for i in input().split(" ")]
+n = vars[0]
+T = vars[1]
+times = [int(i) for i in input().split(" ")]
+
+totTime = T
+counter = 0
+for i in range(times):
+    totTime -= times[i]
+    if totTime >0:
+        counter++
+
