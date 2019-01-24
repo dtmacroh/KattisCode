8cf546cdd48e2c24c272900fd2e7420bec482e23
dtmacroh@ucalgary.ca <dtmacroh@ucalgary.ca>
2019-01-23 17:48:48 -0700

    adding soln primesieve-draft

---
+++ primesieve.py
@@ -0,0 +1,11 @@
+# primesieve
+# Author:		Debbie Macrohon
+# Description:	attempting to use sieve of eratosthenes for this solution
+
+
+vars = [int(i) for i in input().split(" ")]
+n = vars[0]
+q = vars[1]
+
+for i in range(0,q):
+    
