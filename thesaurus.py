a5e3dd04538ed5196d81995badc63f5b96714e3a
dtmacroh <dtmacroh@ucalgary.ca>
2019-01-23 21:41:40 -0700

    in progress

--- thesaurus.py
+++ thesaurus.py
@@ -1,21 +1,35 @@
 # thesaurus
 # Author:		Debbie Macrohon
-# Description:	Find the miniumum number of letters
-#
+# Description:	
+
+def findMin(newPhraseFull,c):
+    newPhraseArray = newPhraseFull.split(" ")
+
+   
+    if newPhraseArray[c] in thesaurus and c >=0:
+        newPhraseArray[c] = thesaurus[newPhraseArray[c]]
+        newPhraseAfter = ''.join(newPhraseArray)
+        
+        return newPhraseAfter
+    return min(len(findMin(newPhraseFull,c-1)),len(findMin(newPhraseAfter,c-1)))
 
 vars = [int(i) for i in input().split(" ")]
 n = vars[0]
 m = vars[1]
-mapping = {}
+thesaurus = {}
 
-phrase = input().split(" ")
+phrase = input()
 for i in range(m):
     thes = input().split(" ")
     key = thes[0]
     val = thes[1]
-    map[key] = val
+    thesaurus[key] = val
 
-def findMin(newPhrase):
-    for c in phrase:
-        if c in mapping
-    return min(findMin(),findMin())
+phrasewords = phrase.split()
+print(thesaurus)
+leastCount = len(''.join(phrasewords))
+for i in phrasewords:
+    res = findMin(phrase,len(phrasewords)-1)
+    if leastCount>len(res):
+        leastCount = len(res)
+print(res,leastCount)
