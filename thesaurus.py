ac24d7a4552ce09554744bd97ddb7cef3021b7f3
dtmacroh <dtmacroh@ucalgary.ca>
2019-01-23 19:58:58 -0700

    rename of dictionary element

--- thesaurus.py
+++ thesaurus.py
@@ -6,18 +6,16 @@
 vars = [int(i) for i in input().split(" ")]
 n = vars[0]
 m = vars[1]
-map = {}
+mapping = {}
 
-phrase = input()
+phrase = input().split(" ")
 for i in range(m):
     thes = input().split(" ")
     key = thes[0]
     val = thes[1]
     map[key] = val
 
-print(map)
-# def findMin(newPhrase):
-#     if len(newPhrase)<len(phrase):
-#         return newPhrase
-#     else:
-#         return min(findMin(),findMin())
+def findMin(newPhrase):
+    for c in phrase:
+        if c in mapping
+    return min(findMin(),findMin())
